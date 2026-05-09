20f5b7352f65036ed2bce25c0bd9afea9d93b909
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2026-05-09 12:40:24 -0500

    fix compilation error(removed import of nonexisting file)

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ViewProvider } from "./context/ViewContext";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 export const metadata = {
   title: "The Campaign Chronicle",
